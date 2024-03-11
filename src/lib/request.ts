04db92c95034abe267fdc0ea29ad28a0c013ec97
fangxi <loveinori@qq.com>
2024-03-11 20:33:04 +0800

    优化一些

--- src/lib/request.ts
+++ src/lib/request.ts
@@ -1,6 +1,13 @@
+/**
+ * Makes an asynchronous HTTP request to the specified URL with the given options.
+ *
+ * @param {string} url - The URL to make the request to
+ * @param {object} options - The options for the HTTP request, including method, body, and params
+ * @return {Promise<any>} A promise that resolves with the JSON response from the server
+ */
 export const request = async (
   url: string,
-  options: { method?: string; body?: any; params?: any } = {}
+  options: { method?: string; body?: any; params?: Record<string, any> } = {},
 ): Promise<any> => {
   const headers: Record<string, string> = {
     "Content-Type": "application/json",
@@ -10,7 +17,7 @@ export const request = async (
 
   let queryParams = "";
   if (params && Object.keys(params).length > 0) {
-    queryParams = new URLSearchParams(params).toString();
+    queryParams = new URLSearchParams(params as Record<string, string>).toString();
   }
 
   const requestOptions: RequestInit = {
@@ -22,28 +29,26 @@ export const request = async (
   }
 
   try {
-    const http_url = `${process.env.NEXT_PUBLIC_API_URL}${url}${
-      queryParams ? `?${queryParams}` : ""
-    }`;
+    const http_url = `${process.env.NEXT_PUBLIC_API_URL}${url}${queryParams ? `?${queryParams}` : ""}`;
     const response = await fetch(http_url, requestOptions);
     if (!response.ok) {
       throw new Error(`HTTP error! status: ${response.status}`);
     }
-    return response.json();
+    return await response.json();
   } catch (error) {
     console.error("Fetch error:", error);
     throw error;
   }
 };
 
-export const ok = (data: any) => {
-  return Response.json({ data: data, ok: true, message: "SUCCESS" });
+export const ok = (data: any): Response => {
+  return Response.json({ data, ok: true, message: "SUCCESS" });
 };
 
-export const fail = (message: string | null) => {
+export const fail = (message: string | null): Response => {
   return Response.json({
     data: null,
     ok: false,
-    message: message == null ? "FAIL" : message,
+    message: message ?? "FAIL",
   });
 };
