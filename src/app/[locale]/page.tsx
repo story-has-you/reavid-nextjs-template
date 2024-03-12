d7579c4c8d58a1844fad2005abdd1e69b25a1dc6
fangxi <loveinori@qq.com>
2024-03-12 16:45:05 +0800

    shadcn init

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,8 +1,13 @@
+import { Button } from "@/components/ui/button";
 import { buildPageLanguage } from "@/config/locale";
 
 const Home = async () => {
   const l = await buildPageLanguage();
-  return <main>{l.title}</main>;
+  return (
+    <main>
+      <Button>{l.title}</Button>
+    </main>
+  );
 };
 
 export default Home;
