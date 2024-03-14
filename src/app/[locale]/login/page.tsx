687bdfa086f627bce6e000d35887369818dc6ed7
fangxi <loveinori@qq.com>
2024-03-14 18:04:38 +0800

    国际化

--- src/app/[locale]/login/page.tsx
+++
@@ -1,7 +0,0 @@
-import { buildPageLanguage } from "@/config/locale";
-import React from "react";
-
-export default async function Login() {
-  const l = await buildPageLanguage();
-  return <div>{l.documentation}</div>;
-}
