a7d03a2a442a5c31687df22b743720144be6b144
fangxi <loveinori@qq.com>
2024-03-18 17:21:23 +0800

    1

--- src/middleware.ts
+++ src/middleware.ts
@@ -11,11 +11,6 @@ export const config = {
   matcher: [
     // Enable a redirect to a matching locale at the root
     "/",
-
-    // Set a cookie to remember the previous locale for
-    // all requests that have a locale prefix
-    "/(en|zh|ja|ko)/:path*",
-
     // Enable redirects that add missing locales
     // (e.g. `/pathnames` -> `/en/pathnames`)
     "/((?!_next|_vercel|.*\\..*).*)",
