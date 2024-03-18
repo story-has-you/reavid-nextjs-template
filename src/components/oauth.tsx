a4175beb93bcb4a32555c5f4075604a8e6bb290b
fangxi <loveinori@qq.com>
2024-03-18 14:32:21 +0800

    login

--- src/components/oauth.tsx
+++ src/components/oauth.tsx
@@ -1,16 +1,26 @@
 import { Icons } from "@/components/icons";
 import { Button } from "@/components/ui/button";
 import { signInWithGithub, signInWithGoogle } from "@/lib/supabase";
+import { signIn } from "next-auth/react";
 import React from "react";
 
 export function Oauth() {
+  const loginGithub = async () => {
+    // await signInWithGithub();
+    signIn("github");
+  };
+
+  const loginGoogle = async () => {
+    // await signInWithGoogle();
+    signIn("google");
+  };
   return (
     <div className="flex flex-col space-y-5 w-full">
-      <Button className="flex items-center justify-center" onClick={signInWithGithub}>
+      <Button className="flex items-center justify-center" onClick={loginGithub}>
         <Icons.gitHub className="h-5 w-5 mr-2" />
         <span>GitHub</span>
       </Button>
-      <Button className="flex items-center justify-center bg-white hover:bg-gray-200" onClick={signInWithGoogle}>
+      <Button className="flex items-center justify-center bg-white hover:bg-gray-200" onClick={loginGoogle}>
         <Icons.google className="h-5 w-5 mr-2" />
         <span className="text-black">Google</span>
       </Button>
