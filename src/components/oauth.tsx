2e00d665373fca62dd9cc555cbe9369a2a6fd5e2
fangxi <loveinori@qq.com>
2024-03-18 10:21:11 +0800

    Google图标

--- src/components/oauth.tsx
+++ src/components/oauth.tsx
@@ -6,12 +6,12 @@ export default function Oauth() {
   return (
     <div className="flex flex-col space-y-5 w-full">
       <Button className="flex items-center justify-center bg-white hover:bg-gray-200">
-        <Icons.gitHub className="h-5 w-5" />
-        <span className="ml-2 text-black">GitHub</span>
+        <Icons.gitHub className="h-5 w-5 mr-2" />
+        <span className="text-black">GitHub</span>
       </Button>
       <Button className="flex items-center justify-center bg-white hover:bg-gray-200">
-        <Icons.google className="h-8 w-8" />
-        <span className="ml-2 text-black">Google</span>
+        <Icons.google className="h-5 w-5 mr-2" />
+        <span className="text-black">Google</span>
       </Button>
     </div>
   );
