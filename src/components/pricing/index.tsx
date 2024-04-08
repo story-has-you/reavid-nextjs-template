6f2c249af2a219798f1515ddb95fd090edbd4fb2
fangxi <loveinori@qq.com>
2024-04-08 23:05:56 +0800

    一些优化。

--- src/components/pricing/index.tsx
+++ src/components/pricing/index.tsx
@@ -3,35 +3,44 @@ import { Icons } from "@/components/icons";
 import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
 
-const freeFeatures = [
-  "Up to 5 project members",
-  "Unlimited tasks and projects",
-  "2 GB storage",
-  "Integrations",
-  "Basic support",
-];
-const proFeatures = [
-  "Up to 50 project members",
-  "Unlimited tasks and projects",
-  "50 GB storage",
-  "Integrations",
-  "Priority support",
-  "Advanced analytics",
-  "Export capabilities",
-];
+const free = {
+  price: "$0",
+  features: [
+    "Up to 50 project members",
+    "Unlimited tasks and projects",
+    "50 GB storage",
+    "Integrations",
+    "Priority support",
+  ],
+};
+const pro = {
+  price: "$15",
+  features: [
+    "Up to 50 project members",
+    "Unlimited tasks and projects",
+    "50 GB storage",
+    "Integrations",
+    "Priority support",
+    "Advanced analytics",
+    "Export capabilities",
+  ],
+};
 
-const businessFeatures = [
-  "Unlimited project members",
-  "Unlimited tasks and projects",
-  "200 GB storage",
-  "Integrations",
-  "Dedicated account manager",
-  "Custom fields",
-  "Advanced analytics",
-  "Export capabilities",
-  "API access",
-  "Advanced security features",
-];
+const business = {
+  price: "$25",
+  features: [
+    "Unlimited project members",
+    "Unlimited tasks and projects",
+    "500 GB storage",
+    "Integrations",
+    "Dedicated account manager",
+    "Custom fields",
+    "Advanced analytics",
+    "Export capabilities",
+    "API access",
+    "Advanced security features",
+  ],
+};
 
 export function Pricing() {
   return (
@@ -47,11 +56,12 @@ export function Pricing() {
         <div className="flex flex-col justify-end items-start p-6 border rounded-lg shadow-lg gap-8 w-1/3">
           <h2 className="text-lg font-semibold opacity-50">Free</h2>
           <p className="text-5xl font-bold">
-            $0<span className="text-sm opacity-50">/month</span>
+            {free.price}
+            <span className="text-sm opacity-50">/month</span>
           </p>
           <Button className="w-full">Get started for free</Button>
           <ul className="mt-5 space-y-5 text-start">
-            {freeFeatures.map((feature) => (
+            {free.features.map((feature) => (
               <li key={feature} className="flex flex-row justify-start items-center">
                 <Icons.check className="text-green-500 mr-2" />
                 {feature}
@@ -61,21 +71,22 @@ export function Pricing() {
         </div>
 
         {/* Pro Plan */}
-        <div className="flex flex-col justify-end items-start p-6 border rounded-lg shadow-lg gap-8 bg-black text-white w-1/3">
+        <div className="flex flex-col justify-end items-start p-6 border rounded-lg shadow-lg gap-8 bg-black text-white dark:bg-white dark:text-black w-1/3">
           <div className="flex flex-row w-full justify-between items-center">
             <h2 className="text-lg font-semibold opacity-50">Pro</h2>
-            <Badge variant={"secondary"} className="text-black">
+            <Badge variant={"secondary"} className="text-primary">
               Most popular
             </Badge>
           </div>
           <p className="text-5xl font-bold">
-            $9<span className="text-sm">/month</span>
+            {pro.price}
+            <span className="text-sm">/month</span>
           </p>
-          <Button variant="outline" className="w-full bg-white text-black">
+          <Button variant="outline" className="w-full bg-white text-black dark:bg-black dark:text-white">
             Sign up now
           </Button>
           <ul className="mt-5 space-y-5 text-start">
-            {proFeatures.map((feature) => (
+            {pro.features.map((feature) => (
               <li key={feature} className="flex flex-row justify-start items-center">
                 <Icons.check className="text-green-500 mr-2" />
                 {feature}
@@ -88,11 +99,12 @@ export function Pricing() {
         <div className="flex flex-col justify-end items-start p-6 border rounded-lg shadow-lg gap-8 w-1/3">
           <h2 className="text-lg font-semibold opacity-50">Business</h2>
           <p className="text-5xl font-bold">
-            $19<span className="text-sm">/month</span>
+            {business.price}
+            <span className="text-sm">/month</span>
           </p>
           <Button className="w-full">Sign up now</Button>
           <ul className="mt-5 space-y-5 text-start">
-            {businessFeatures.map((feature) => (
+            {business.features.map((feature) => (
               <li key={feature} className="flex flex-row justify-start items-center">
                 <Icons.check className="text-green-500 mr-2" />
                 {feature}
