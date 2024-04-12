62d3a9450aeb0cf48baad211b397373e8fcf2f59
fangxi <loveinori@qq.com>
2024-04-12 17:35:00 +0800

    1

--- src/components/prompt-form/actions.ts
+++ src/components/prompt-form/actions.ts
@@ -1,18 +1,47 @@
 "use server";
 
-import { nanoid } from "@/lib/utils";
+import { getServerUserId, nanoid } from "@/lib/utils";
+import { revalidateTag } from "next/cache";
 import { redirect } from "next/navigation";
+import { z } from "zod";
 
-export const action = async (prevState: boolean, formData: FormData): Promise<boolean> => {
-  const prompt = (formData.get("prompt") as string | null)?.trim().replaceAll(":", "");
+const formSchema = z.object({
+  prompt: z.string().min(2, {
+    message: "Prompt must be at least 2 characters.",
+  }),
+});
 
-  if (!prompt) {
-    return false;
+interface FormMessage {
+  validatedErrors?: z.inferFlattenedErrors<typeof formSchema>["fieldErrors"] | null;
+  message?: string | null;
+}
+
+export const action = async (prevState: FormMessage | undefined, formData: FormData) => {
+  const rawFormData = {
+    prompt: (formData.get("prompt") as string | null)?.trim().replaceAll(":", ""),
+  };
+  const validatedFields = formSchema.safeParse(rawFormData);
+  if (!validatedFields.success) {
+    return {
+      validatedErrors: validatedFields.error.flatten().fieldErrors,
+    };
   }
+
+  const userId = await getServerUserId();
+  if (!userId) {
+    return {
+      message: "Unauthorized",
+    };
+  }
+
+  const { prompt } = validatedFields.data;
+
   const id = nanoid();
   try {
   } catch (error) {
     console.error(error);
+    return { message: "Form Action Error" };
   }
+  revalidateTag("p");
   redirect(`/p/${id}`);
 };
