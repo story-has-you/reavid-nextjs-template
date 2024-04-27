5193d3944d637f633c9e5b2d2aacf2c1e426d602
fangxi <loveinori@qq.com>
2024-04-27 13:42:15 +0800

    sql 变更

---
+++ prisma/init.sql
@@ -0,0 +1,4 @@
+INSERT INTO "public"."vip" ("id", "month_price", "year_price", "name") VALUES
+('LHtUjv2sDUi390awsmshi92O9cCzMoRw', 50.00, 500.00, 'Business'),
+('XvpXevuWxGJA2RA3Nl7rIzI53pxO5o3W', 20.00, 200.00, 'Pro'),
+('ekOJd96f40Aj10qT8kNIpaOnwhvdoeVx', 0.00, 0.00, 'Free');
