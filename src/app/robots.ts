1be83468ff9219cf971df54b59bd692e7dbd0a6e
fangxi <loveinori@qq.com>
2024-04-30 17:20:53 +0800

    优化

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -6,7 +6,7 @@ export default function robots(): MetadataRoute.Robots {
     rules: {
       userAgent: "*",
       allow: "/",
-      disallow: "/private/",
+      disallow: "/api",
     },
     sitemap: `${siteConfig.url}/sitemap.xml`,
   };
